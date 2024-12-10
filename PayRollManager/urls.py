5e3a94eb40fcbbfc250a0c1267cd2677b957180a
osolenye <116378424+osolenye@users.noreply.github.com>
2024-12-11 00:16:30 +0600

    swagger added

--- PayRollManager/urls.py
+++ PayRollManager/urls.py
@@ -1,7 +1,27 @@
 from django.contrib import admin
 from django.urls import path, include
 
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+
+# Настройка для Swagger
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Your API",
+      default_version='v1',
+      description="API Documentation",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="contact@yourdomain.com"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+)
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),  # Подключение маршрутов из приложения api
+
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0),
+         name='schema-swagger-ui'),
 ]
